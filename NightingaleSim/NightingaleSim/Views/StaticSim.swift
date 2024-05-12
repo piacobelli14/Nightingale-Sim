774922bc679583631ddec251a630efc4404a4c78
piacobelli14 <dinolabsauthentication@outlook.com>
2024-05-12 18:53:37 -0500

    update

--- NightingaleSim/NightingaleSim/Views/StaticSim.swift
+++ NightingaleSim/NightingaleSim/Views/StaticSim.swift
@@ -471,7 +471,7 @@ struct StaticSim: View {
                         
                         VStack {
                             Button(action: {
-                                self.appView = .SettingsSim
+                                self.AppView = .SettingsSim
                             }) {
                                 Image(systemName: "gear")
                                     .resizable()
