ecb9ee74a820bab67c6c7a1ac543e735a2aaaebd
piacobelli14 <dinolabsauthentication@outlook.com>
2024-05-13 08:27:47 -0500

    uhpdate

--- NightingaleSim/NightingaleSim/Views/SettingsSim.swift
+++ NightingaleSim/NightingaleSim/Views/SettingsSim.swift
@@ -18,6 +18,106 @@ struct SettingsSim: View {
     )
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack {
+                HStack {
+                    Image("nslogo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: geometry.size.height * 0.07)
+                        .shadow(color: .gray.opacity(0.5), radius: 3, x: 0, y: 0)
+                        .padding(.leading, geometry.size.width * 0.01)
+                    
+                    Text("Nightinagle Sim")
+                        .font(.system(size: geometry.size.height * 0.03, weight: .bold))
+                        .foregroundColor(.white)
+                        .shadow(color: .gray.opacity(0.3), radius: 0, x: 0, y: 2)
+                    
+                    Spacer()
+                }
+                .frame(width: geometry.size.width * 1.0)
+                .padding(.top, geometry.size.height * 0.05)
+                .edgesIgnoringSafeArea(.all)
+                .background(
+                    VStack {
+                        Spacer()
+                        Rectangle()
+                            .frame(height: geometry.size.height * 0.001)
+                            .foregroundColor(.gray)
+                    }
+                )
+                
+                ScrollView {
+                    
+                }
+                
+                Spacer()
+                
+                HStack(alignment: .center) {
+                    
+                    Spacer()
+                    
+                    HStack {
+                        
+                        VStack {
+                            Button(action: {
+                                self.currentView = .StaticSim
+                            }) {
+                                Image(systemName: "hourglass.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: geometry.size.height * 0.022)
+                                    .foregroundColor(Color.white)
+                                    .shadow(color: .gray.opacity(0.5), radius: 3, x: 0, y: 0)
+                                    .padding(.leading, geometry.size.width * 0.01)
+                            }
+                            
+                            Text("Simulator")
+                                .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
+                                .foregroundColor(Color.white)
+                                .opacity(0.6)
+                        }
+                        .padding(.trailing, geometry.size.width * 0.01)
+                        
+                        VStack {
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "lock")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: geometry.size.height * 0.022)
+                                    .foregroundColor(Color.white)
+                                    .shadow(color: .gray.opacity(0.5), radius: 3, x: 0, y: 0)
+                                    .padding(.leading, geometry.size.width * 0.01)
+                            }
+                            
+                            Text("Logout")
+                                .font(.system(size: geometry.size.height * 0.012, weight: .semibold))
+                                .foregroundColor(Color.white)
+                                .opacity(0.6)
+                        }
+                        .padding(.trailing, geometry.size.width * 0.03)
+                    }
+                    .padding(.top, geometry.size.height * 0.01)
+                    .padding(.bottom, geometry.size.height * 0.01)
+                    
+                }
+                .frame(width: geometry.size.width * 1.0)
+                .edgesIgnoringSafeArea(.all)
+                .background(Color(hex: 0x504F51))
+                .background(
+                    VStack {
+                        Rectangle()
+                            .frame(height: geometry.size.height * 0.001)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                )
+                
+            }
+            .frame(width: geometry.size.width * 1.0, height: geometry.size.height * 1.0)
+            .background(gradient)
+        }
     }
 }
