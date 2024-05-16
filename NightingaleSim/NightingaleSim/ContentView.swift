1d1bb9356960622f8484f70fd822baeb3ae5ae74
piacobelli14 <dinolabsauthentication@outlook.com>
2024-05-15 20:05:34 -0500

    update

--- NightingaleSim/NightingaleSim/ContentView.swift
+++ NightingaleSim/NightingaleSim/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentView: AppView = .SettingsSim
+    @State private var currentView: AppView = .LoginAuth
     @State private var authenticatedUsername: String = ""
     @State private var authenticatedOrgID: String = ""
     @State private var isMotion: Bool = true
