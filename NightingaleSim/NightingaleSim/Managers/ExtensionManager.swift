7985ee9e4202b9c5007caf37421f47719d8025f4
piacobelli14 <dinolabsauthentication@outlook.com>
2024-06-11 15:06:22 -0500

    update

---
+++ NightingaleSim/NightingaleSim/Managers/ExtensionManager.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionManager.swift
+//  NightingaleSim
+//
+//  Created by Peter Iacobelli on 6/11/24.
+//
+
+import Foundation
---
+++ NightingaleSim/NightingaleSim/Managers/SecurityManager.swift
@@ -0,0 +1,8 @@
+//
+//  SecurityManager.swift
+//  NightingaleSim
+//
+//  Created by Peter Iacobelli on 6/11/24.
+//
+
+import Foundation
